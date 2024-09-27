710bf12168cf765af01b0f9a46b27bb43b355bde
pntloi <loipham12345@gmail.com>
2024-09-27 23:59:00 +0800

    Draw road

--- Main.py
+++ Main.py
@@ -8,19 +8,18 @@ from matplotlib.patches import Rectangle
 #  this is the func that call all other stuffs to get things done
 def main():
     # define the w and h parameters
-    x_lim = 150
-    y_lim = 170
     
     fig, axis = plt.subplots(ncols=2)
     
     ax1, ax2 = axis
     
-    land_RGB = Land(x_lim, y_lim)
+    land_RGB = Land()
     landview_RGB = land_RGB.generate_land()
     
     land_RGB.generate_park(ax1) # Generate park
     land_RGB.generate_residential_area(ax1) # Generate residential area
     
+    land_RGB.generate_road_line(ax1)
     
     
     im1 = ax1.imshow(landview_RGB)
--- canopy.py
+++ canopy.py
@@ -16,7 +16,7 @@ def swap_coords(x, y):
     return (y, x)
 
 class Land():
-    def __init__(self, x_lim, y_lim, colour=None):
+    def __init__(self, x_lim=180, y_lim=180, colour=None):
         self.x_lim = x_lim
         self.y_lim = y_lim
         self.colour = ["red", "green", "blue"]
@@ -26,21 +26,42 @@ class Land():
         land = np.zeros((self.y_lim, self.x_lim))
         return land
     
-    def generate_park(self, axis):
-        width = (self.x_lim)//2 - 10
-        height = self.y_lim
-        axis.add_patch(Rectangle((0, 0), width, height, 
-                        fill=True,
-                        color=self.colour[1],
-                        linewidth=2))
-        
     def generate_residential_area(self, axis):
         width = (self.x_lim)//2 - 10
-        height = self.y_lim
-        axis.add_patch(Rectangle(((self.x_lim)//2 + 10, 0), width, height, 
-                        fill=True,
-                        color=self.colour[2],
-                        linewidth=2))
+        for part in range(0, self.y_lim, 3):
+            if part % 2 == 1:
+                axis.add_patch(Rectangle((0, part), width, 10, 
+                                fill=True,
+                                color=self.colour[2],
+                                linewidth=3))
+            else:
+                width -= 0.5
+                axis.add_patch(Rectangle((0, part), width, 10, 
+                                fill=True,
+                                color=self.colour[2],
+                                linewidth=3))
+
+        
+    def generate_park(self, axis):
+        width = (self.x_lim)//2 - 10
+        starting_point = (self.x_lim)//2 + 10
+        for part in range(self.y_lim + 1, 0, -3):
+            if part % 2 == 1:
+                axis.add_patch(Rectangle((starting_point, part), width, 10, 
+                                fill=True,
+                                color=self.colour[1],
+                                linewidth=3))
+            else:
+                width -= - 0.5
+                starting_point += 0.5
+                axis.add_patch(Rectangle((starting_point, part), width, 10, 
+                                fill=True,
+                                color=self.colour[1],
+                                linewidth=3))
+        
+    def generate_road_line(self, axis):
+        road = Road(axis, 25)
+        road.draw_line()
         
     def __str__(self):
         return f"The initial temperature is {self.initialTemp} degree. And the width of the land is {self.x_lim} meters and the height of the land is {self.y_lim} meters"
@@ -120,12 +141,26 @@ class Tree(StaticObject):
     myclass = "Tree"
     pass
 
-class Road(StaticObject):
+class Road():
     myclass = "Road"
     type = 3
-    def __init__(self, x, y, axis, colour, objTemp):
-        super().__init__(colour, objTemp)
-        axis[x//2, 0] = self.type
+    def __init__(self, axis, objTemp):
+        self.axis = axis
+        self.objTemp = objTemp
+        
+    def draw_line(self):
+        x_rl = [98, 84]
+        y_rl = [0, 180]
+        
+        x_left_road = [81, 67]
+        y_left_road = [0, 180]
+        x_right_road = [115, 100]
+        y_right_road = [0, 180]
+        
+        self.axis.plot(x_rl, y_rl, 'g--', color="white")
+        self.axis.plot(x_left_road, y_left_road, color="black")
+        self.axis.plot(x_right_road, y_right_road, color="black")
+        
         
         
 class Car(StaticObject):
